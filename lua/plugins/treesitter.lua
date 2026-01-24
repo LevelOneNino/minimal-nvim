1d551290c00d9e87b36f69704ec265c0bd23d671
Diego Andrés Niño Rendón <daninorendon@gmail.com>
2026-01-23 19:43:25 -0500

    uptade

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,27 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-      auto_install = true,
-      ignore_install = {
-      },
-      highlight = {
-        enable = true,
-        disable = {
-          "pug",
-          "html",
-        },
-      },
-      indent = {
-        enable = true,
-        disable = {
-          "pug",
-          "html",
-        },
-      },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	lazy = false,
+	config = function()
+		require('nvim-treesitter').setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+			auto_install = true,
+			ignore_install = {
+			},
+			highlight = {
+				enable = true,
+				disable = {
+					"pug",
+					"html",
+				},
+			},
+			indent = {
+				enable = true,
+				disable = {
+					"pug",
+					"html",
+				},
+			},
+		})
+	end,
 }
